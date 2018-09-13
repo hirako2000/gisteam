676be1edbd7b7d4635f62e52a301f458741a1a10
hirako2000 <hirako2000@gmail.com>
2018-09-13 15:55:01 +0700

    Fixes some checkstyles

--- src/routes/decode/index.js
+++ src/routes/decode/index.js
@@ -1,5 +1,5 @@
-import {app} from 'server';
-import {post, get} from 'koa-route';
+import { app } from 'server';
+import { post, get } from 'koa-route';
 import template from './template.marko';
 
 const url = 'URL';
@@ -9,28 +9,37 @@ const formats = [url, base64];
 
 const title = 'Decode - GisTeam';
 
-app.use(get('/decode', async (ctx, next) => {
-  ctx.body = template.stream({formats, title});
-  ctx.type = 'text/html';
-}));
+app.use(
+  get('/decode', async (ctx, next) => {
+    ctx.body = template.stream({ formats, title });
+    ctx.type = 'text/html';
+  })
+);
 
-app.use(post('/decode', async (ctx, next) => {
-  const format = ctx.request.body.format;
-  const input = ctx.request.body.input;
-  let output;
+app.use(
+  post('/decode', async (ctx, next) => {
+    const format = ctx.request.body.format;
+    const input = ctx.request.body.input;
+    let output;
 
-  switch (format) {
-    case url:
-      output = decodeURI(input);
-      break;
-    case base64:
-      output = Buffer.from(input, 'base64').toString()
-      break;
-    default:
-      output = decodeURI(input);
-  }
+    switch (format) {
+      case url:
+        output = decodeURI(input);
+        break;
+      case base64:
+        output = Buffer.from(input, 'base64').toString();
+        break;
+      default:
+        output = decodeURI(input);
+    }
 
-  ctx.body = template.stream({format, formats, input, result: output, title});
-  ctx.type = 'text/html';
-}));
-
+    ctx.body = template.stream({
+      format,
+      formats,
+      input,
+      result: output,
+      title
+    });
+    ctx.type = 'text/html';
+  })
+);
--- src/routes/encode/index.js
+++ src/routes/encode/index.js
@@ -1,5 +1,5 @@
-import {app} from 'server';
-import {post, get} from 'koa-route';
+import { app } from 'server';
+import { post, get } from 'koa-route';
 import template from './template.marko';
 
 const url = 'URL';
@@ -9,28 +9,37 @@ const formats = [url, base64];
 
 const title = 'Encode - GisTeam';
 
-app.use(get('/encode', async (ctx, next) => {
-  ctx.body = template.stream({formats, title});
-  ctx.type = 'text/html';
-}));
+app.use(
+  get('/encode', async (ctx, next) => {
+    ctx.body = template.stream({ formats, title });
+    ctx.type = 'text/html';
+  })
+);
 
-app.use(post('/encode', async (ctx, next) => {
-  const format = ctx.request.body.format;
-  const input = ctx.request.body.input;
-  let output;
+app.use(
+  post('/encode', async (ctx, next) => {
+    const format = ctx.request.body.format;
+    const input = ctx.request.body.input;
+    let output;
 
-  switch (format) {
-    case url:
-      output = encodeURI(input);
-      break;
-    case base64:
-      output = Buffer.from(input).toString('base64')
-      break;
-    default:
-      output = encodeURI(input);
-  }
+    switch (format) {
+      case url:
+        output = encodeURI(input);
+        break;
+      case base64:
+        output = Buffer.from(input).toString('base64');
+        break;
+      default:
+        output = encodeURI(input);
+    }
 
-  ctx.body = template.stream({format, formats, input, result: output, title});
-  ctx.type = 'text/html';
-}));
-
+    ctx.body = template.stream({
+      format,
+      formats,
+      input,
+      result: output,
+      title
+    });
+    ctx.type = 'text/html';
+  })
+);
